69d448883283a1729171b89b050634b9cf67650c
Hamlet Morales Luis <Hamlet Morales Luis>
2019-06-12 09:29:56 -0500

    chante port to avoid conflict on the server

--- config/secret.js
+++ config/secret.js
@@ -3,7 +3,7 @@ module.exports = {
  // database: process.env.DATABASE || 'mongodb://root:webmaster123@fyned-test-shard-00-00-kzw6z.mongodb.net:27017,fyned-test-shard-00-01-kzw6z.mongodb.net:27017,fyned-test-shard-00-02-kzw6z.mongodb.net:27017/test?ssl=true&replicaSet=fyned-test-shard-0&authSource=admin&retryWrites=true',
 //database: process.env.DATABASE || 'mongodb://root:webmaster123@fyned-test-shard-00-00-kzw6z.mongodb.net:27017,fyned-test-shard-00-01-kzw6z.mongodb.net:27018,fyned-test-shard-00-02-kzw6z.mongodb.net:27017/test?ssl=true&replicaSet=fyned-test-shard-0&authSource=admin&retryWrites=true',
 database: process.env.DATABASE || 'mongodb://localhost/fyned_admin_db',
-  port: process.env.PORT || 3007,
+  port: process.env.PORT || 3003,
   secret: process.env.SECRET || 'fiverclone2222',
 
 }
