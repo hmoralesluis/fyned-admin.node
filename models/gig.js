5955af36e078f1da1097d7055d8a7a7d36c6efd8
Hamlet <hmoralesluis@gmail.com>
2019-07-08 14:55:32 -0500

    Sugerencias en los platos

--- models/gig.js
+++ models/gig.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-// const mongooseAlgolia = require('mongoose-algolia');
+const mongooseAlgolia = require('mongoose-algolia');
 const Schema = mongoose.Schema;
 
 const GigSchema = new Schema({
@@ -19,33 +19,33 @@ const GigSchema = new Schema({
   sugerencia: {type: Boolean, default: true},
 });
 
-// GigSchema.plugin(mongooseAlgolia,{
-//   appId: 'L3E3RMHJBU',
-//   apiKey: '9cb4a910e7442c50a8758bdb02e40d1c',
-//   indexName: 'GigSchema', //The name of the index in Algolia, you can also pass in a function
-//   selector: 'title _id owner category about price picture', //You can decide which field that are getting synced to Algolia (same as selector in mongoose)
-//   populate: {
-//     path: 'owner',
-//     select: 'name'
-//   },
-//   defaults: {
-//     author: 'unknown'
-//   },
-//   mappings: {
-//     title: function(value) {
-//       return `Title: ${value}`
-//     }
-//   },
-//   debug: true // Default: false -> If true operations are logged out in your console
-// });
-//
-//
+GigSchema.plugin(mongooseAlgolia,{
+  appId: 'L3E3RMHJBU',
+  apiKey: '9cb4a910e7442c50a8758bdb02e40d1c',
+  indexName: 'GigSchema', //The name of the index in Algolia, you can also pass in a function
+  selector: 'title _id owner category about price picture', //You can decide which field that are getting synced to Algolia (same as selector in mongoose)
+  populate: {
+    path: 'owner',
+    select: 'name'
+  },
+  defaults: {
+    author: 'unknown'
+  },
+  mappings: {
+    title: function(value) {
+      return `Title: ${value}`
+    }
+  },
+  debug: true // Default: false -> If true operations are logged out in your console
+});
 
-//
-// Model.SyncToAlgolia(); //Clears the Algolia index for this schema and synchronizes all documents to Algolia (based on the settings defined in your plugin settings)
-// Model.SetAlgoliaSettings({
-//   searchableAttributes: ['title','owner.name'] //Sets the settings for this schema, see [Algolia's Index settings parameters](https://www.algolia.com/doc/api-client/javascript/settings#set-settings) for more info.
-// });
 
 let Model = mongoose.model('Gig', GigSchema);
+
+Model.SyncToAlgolia(); //Clears the Algolia index for this schema and synchronizes all documents to Algolia (based on the settings defined in your plugin settings)
+Model.SetAlgoliaSettings({
+  searchableAttributes: ['title','owner.name'] //Sets the settings for this schema, see [Algolia's Index settings parameters](https://www.algolia.com/doc/api-client/javascript/settings#set-settings) for more info.
+});
+
+
 module.exports = Model;
